310d98eb6d4cdde76863989afda9ba86fd2e46c1
suparnabose30 <43331187+suparnabose30@users.noreply.github.com>
2019-06-11 15:16:14 +0530

    Add files via upload

---
+++ src/main/java/com/example/openshiftdemomaven/OpenshiftdemomavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.openshiftdemomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftdemomavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftdemomavenApplication.class, args);
+	}
+
+}
